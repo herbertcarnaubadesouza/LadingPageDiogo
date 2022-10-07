568807273e156aded47f42e82e013d14637403a6
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-10-06 23:46:41 -0300

    solvind problems again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
-export const version = '1.0'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
