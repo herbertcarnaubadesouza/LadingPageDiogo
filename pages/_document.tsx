9a6adb1e76c5b281a8e2f3805ffc4a9dd9bcbf35
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-10-07 09:18:03 -0300

    correcting console errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,14 +9,9 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
 
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,500;1,900&display=swap"
             rel="stylesheet"
           />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@200;300;400;600;700&display=swap"
-            rel="stylesheet"
-          ></link>
           <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
         </Head>
         <body>
