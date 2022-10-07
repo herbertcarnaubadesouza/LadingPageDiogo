917df239429ef59a2b4dc67c54125013d4d54618
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-10-06 23:43:50 -0300

    solving problems

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@200;300;400;600;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
