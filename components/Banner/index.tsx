6d10a22930ff6fbacad47ad8aedf4a5cff362800
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-10-19 17:18:41 -0300

    feat: ajusting button

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -4,12 +4,19 @@ export function Banner() {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h2>Encontre seu plano de saúde ideal.</h2>
-        <span>
-          Assim, você pode cuidar da sua saúde com tranquilidade e segurança.
-        </span>
+        <h1>
+          Economize tempo e encontre o plano de saúde ideal para você, sua
+          família ou empresa.
+        </h1>
+        <h2>
+          Com a minha assessoria gratuita você descobre o plano de saúde com o
+          melhor custo-benefício de acordo com a sua necessidade, conhece todas
+          as vantagens que ele te oferece, conta com um suporte especializado
+          para a contratação e fica longe da burocracia dos atendimentos via SAC
+          e 0800 das operadoras. Assim, você pode cuidar da sua saúde com
+          tranquilidade e segurança.
+        </h2>
       </div>
-      <img className={styles.bannerImg} src="/images/banner.png"></img>
     </section>
   )
 }
--- components/Me/index.tsx
+++ components/Me/index.tsx
@@ -1,5 +1,39 @@
 import styles from './styles.module.scss'
 
 export function MobileHeader() {
-  return <section className={styles.container}></section>
+  return (
+    <section className={styles.container}>
+      <div className={styles.container}>
+        <img src="/images/Diogo.jpg" alt="Corretor de seguros" />
+        <div className={styles.aboutContent}>
+          <h2>Conheça o seu novo corretor</h2>
+          <span className={styles.divisor}></span>
+          <p style={{ marginTop: '3rem' }}>
+            Diogo Santos, tenho 38 anos, sou Corretor de Seguros especializado
+            em Planos de Saúde.
+          </p>
+          <p>
+            Trabalho na área há 12 anos e já atendi mais de 1.2.500 vidas,
+            gerando aproximadamente 350 mil em redução de custos e economia aos
+            meus clientes
+          </p>
+          {/* <ul className={styles.statsList}>
+            <li>
+              <p className={styles.statsTitle}>9875</p>
+              <p className={styles.statsText}>Satisfied Clients</p>
+            </li>
+            <li>
+              <p className={styles.statsTitle}>7894</p>
+              <p className={styles.statsText}>Project Lunched</p>
+            </li>
+            <li>
+              <p className={styles.statsTitle}>65</p>
+              <p className={styles.statsText}>Years Completed</p>
+            </li>
+          </ul> */}
+          <button>Fale Comigo</button>
+        </div>
+      </div>
+    </section>
+  )
 }
